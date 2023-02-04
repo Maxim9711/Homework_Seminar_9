e5637395ecc581f2298e3de7f63e847e60c87873
Maxim <Bruce.97@mail.ru>
2023-02-04 18:40:43 +0700

    task 64

---
+++ Задача_64/Program.cs
@@ -0,0 +1,20 @@
+﻿// See https://aka.ms/new-console-template for more information
+void NumberToConsole(int N){
+
+  if (N > 1)
+    Console.Write($"{N}, ");
+  else
+    Console.WriteLine($"{N}");
+
+  if (N > 1)
+    NumberToConsole(N-1);
+
+}
+
+
+
+
+Console.Write("Введите N:");
+int N = Convert.ToInt32(Console.ReadLine());
+
+NumberToConsole(N);
